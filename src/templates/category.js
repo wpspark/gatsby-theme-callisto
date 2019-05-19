64013a1f3a3a2fe0c7c0d5a597492badf00b916d
Abu Huraira <shams.bd71@gmail.com>
2019-05-19 15:10:16 +0600

    category page done

--- src/templates/category.js
+++ src/templates/category.js
@@ -2,17 +2,22 @@ import React, { Component } from 'react'
 import Layout from "../layouts"
 import SEO from "../utils/seo"
 import AllPost from "../components/all-post"
+import PageTitle from "../components/page-title"
 
 
 class CategoryPage extends Component {
     
   render() {
   	const data = this.props.data;
+  	const category = data.wordpressCategory;
+  	console.log(this.props)
 
     return (
         <Layout>
         	<SEO title="Category Page" />
         	
+        	<PageTitle title={category.name} subtitle={category.description} />
+
         	<AllPost data={data.allWordpressPost} />
 
         </Layout>
@@ -24,6 +29,14 @@ export default CategoryPage
 
 export const categoryQuery = graphql`
     query currentCategoryQuery($slug: String!) {
+
+    	wordpressCategory(slug: { eq: $slug }){
+		    id
+		    name
+            slug
+            count
+            description
+	  	}
 
         allWordpressPost(filter: {
             categories: {
