b384ca4f8224727503e6de50e6fc70760297400c
Abu Huraira <shams.bd71@gmail.com>
2019-05-21 15:53:54 +0600

    404 page build fixed

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,24 +1,39 @@
 import React, { Component } from 'react'
 import Layout from "../layouts"
+import SEO from "../utils/seo"
 
 class NotFoundPage extends Component {
     
   render() {
     return (
-        <Layout>
+			<Layout wordpressSiteMetadata={this.props.data.wordpressSiteMetadata}>
+					<SEO title="Page not found" />
         	<section className="hero is-warning">
-			  <div className="hero-body">
-			      <h1 className="title">
-			        404
-			      </h1>
-			      <h2 className="subtitle">
-			        Page not found!
-			      </h2>
-			  </div>
-			</section>
+						<div className="hero-body">
+								<h1 className="title">
+									404
+								</h1>
+								<h2 className="subtitle">
+									Page not found!
+								</h2>
+						</div>
+					</section>
         </Layout>
     )
   }
 }
 
 export default NotFoundPage
+
+export const categoryQuery = graphql`
+  query wordpressSiteMetadata{
+
+		wordpressSiteMetadata{
+			name
+			description
+			url
+			home
+		}
+    
+  }
+`
