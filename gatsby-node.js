1a11dff49e4014fb0d5c1e8e91259b3f1733ba7b
Abu Huraira <shams.bd71@gmail.com>
2019-05-22 17:22:58 +0600

    gatsby node file name case fixed

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,8 +1,8 @@
 const createPosts = require(`./src/routing/createposts`)
 const createCategories = require(`./src/routing/createcategories`)
-// const createPages = require(`./src/routing/createPages`)
-// const createUsers = require(`./src/routing/createUsers`)
-// const createTags = require(`./src/routing/createTags`)
+// const createPages = require(`./src/routing/createpages`)
+// const createUsers = require(`./src/routing/createusers`)
+// const createTags = require(`./src/routing/createtags`)
 
 exports.createPages = async ({ actions, graphql }) => {
   await createPosts({ actions, graphql })
