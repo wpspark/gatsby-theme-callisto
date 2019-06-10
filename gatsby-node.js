6f881b56aaddc8239ec3f043f26d8163d1b89c2d
Iftekher Sunny <iftekhersunny@gmail.com>
2019-06-10 12:26:12 +0600

    uses graphql for sitedata api

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,3 +1,4 @@
+const axios = require('axios');
 const createPosts = require(`./src/routing/createPosts`)
 const createCategories = require(`./src/routing/createCategories`)
 // const createPages = require(`./src/routing/createpages`)
@@ -10,4 +11,30 @@ exports.createPages = async ({ actions, graphql }) => {
   // await createPages({ actions, graphql })
   // await createUsers({ actions, graphql })
   // await createTags({ actions, graphql })
+}
+
+exports.sourceNodes = async ({ actions, createNodeId, createContentDigest }) => {
+  const url = `${process.env.protocol}://${process.env.baseUrl}`;
+  const result = await axios.get(`${url}/wp-json/spark/sitedata`);
+  const data = result.data;
+
+  const dataObject = {
+    "favicon": data.favicon,
+    "logo": data.logo
+  };
+
+  const nodeContent = JSON.stringify(dataObject)
+  const nodeMeta = {
+    id: createNodeId(`spark-site-info`),
+    parent: null,
+    children: [],
+    internal: {
+      type: `sparkData`,
+      content: nodeContent,
+      contentDigest: createContentDigest(dataObject)
+    }
+  }
+
+  const node = Object.assign({}, dataObject, nodeMeta)
+  actions.createNode(node)
 }
--- src/components/siteHeader/index.js
+++ src/components/siteHeader/index.js
@@ -1,108 +1,86 @@
 import React, { Component } from 'react'
-import { Link } from "gatsby"
-import axios from "axios"
+import { Link, StaticQuery, graphql } from "gatsby"
 // import Menu from "../siteMenu"
 import CategoryMenu from "../category-menu/index"
 // import logo from "../../images/wpspark-logo.png"
 import Helmet from "react-helmet"
 
 export default class Header extends Component {
-    state = {
-      loading: false,
-      favicon: '',
-      logo: '',
-    }
-
     toggleDropdownMenu = () => {
       document.getElementById('MainsiteNav').classList.toggle('is-active');
-    }
-
-    componentDidMount() {
-      this.featchWpSparkData()
-    }
-
-    featchWpSparkData(){
-      if(this.props.wordpressSiteMetadata === undefined) return true;
-      
-      let url = this.props.wordpressSiteMetadata.url;
-      // this.setState({ loading: true })
-      if(!window.siteData) {
-        axios
-          .get(`${url}/wp-json/spark/sitedata`)
-          .then(pupper => {
-            let dataObject = pupper.data[0];
-
-            const favicon = dataObject.icon
-            const logo = dataObject.logo
-
-            window.siteData = {
-              loading: true,
-              favicon,
-              logo
-            }
-
-            this.setState(window.siteData)
-          })
-          .catch(error => {
-            this.setState({ loading: false })
-          })
-      } else {
-        this.setState(window.siteData)
-      }
     }
 
     render() {
       let wordpressSiteMetadata = this.props.wordpressSiteMetadata;
 
       return (
-        <nav className="navbar is-transparent has-shadow is-spaced is-fixed-top" role="navigation">
-          <Helmet
-              link={[
-                {
-                  "rel": "icon", 
-                  "type": "image/png", 
-                  "href": this.state.favicon
-                }
-              ]}
-          />
-          <div className="navbar-brand">
-            
-            <Link to="/" className="navbar-item">
-              
-              {
-                this.state.loading ? <img src={this.state.logo} alt="" width="112" height="28" /> : 
-                wordpressSiteMetadata.name
-              }
+        <StaticQuery
+          query={graphql`
+          query sparkDataFromSite {
+              sparkData {
+                logo 
+                favicon
+              } 
+            }
+          `}
+          render={data => {
+            const { sparkData } = data;
 
-            </Link>
+            return <nav className="navbar is-transparent has-shadow is-spaced is-fixed-top" role="navigation">
+              <Helmet
+                link={[
+                  {
+                    "rel": "icon",
+                    "type": "image/png",
+                    "href": sparkData.favicon
+                  }
+                ]}
+              />
+              <div className="navbar-brand">
 
-            <span className="navbar-burger burger" data-target="MainsiteNav" onClick={this.toggleDropdownMenu}>
-              <span/><span/><span/>
-            </span>
-          </div>
+                <Link to="/" className="navbar-item">
 
-          <div id="MainsiteNav" className="navbar-menu">
-            <div className="navbar-start">
-              
-              <CategoryMenu slug={this.props.slug}/>
+                  {/* {
+                    this.state.loading ? <img src={this.state.logo} alt="" width="112" height="28" /> :
+                      wordpressSiteMetadata.name
+                  } */}
+                  
+                  {
+                    sparkData.logo ? <img src={sparkData.logo} alt="" width="112" height="28" /> :
+                      wordpressSiteMetadata.name
+                  }
 
-            </div>
+                </Link>
 
-            <div className="navbar-end">
-              {/* <Menu data={this.props.pageContext} /> */}
-              
-              <div className="navbar-item">
-                <div className="field is-grouped">
-                  <p className="control">
-                    <a className="button is-primary" href={wordpressSiteMetadata.url} target="_blank" rel="noopener noreferrer">
-                        <span>Mainsite</span>
-                    </a>
-                  </p>
+                <span className="navbar-burger burger" data-target="MainsiteNav" onClick={this.toggleDropdownMenu}>
+                  <span /><span /><span />
+                </span>
+              </div>
+
+              <div id="MainsiteNav" className="navbar-menu">
+                <div className="navbar-start">
+
+                  <CategoryMenu slug={this.props.slug} />
+
+                </div>
+
+                <div className="navbar-end">
+                  {/* <Menu data={this.props.pageContext} /> */}
+
+                  <div className="navbar-item">
+                    <div className="field is-grouped">
+                      <p className="control">
+                        <a className="button is-primary" href={wordpressSiteMetadata.url} target="_blank" rel="noopener noreferrer">
+                          <span>Mainsite</span>
+                        </a>
+                      </p>
+                    </div>
+                  </div>
                 </div>
               </div>
-            </div>
-          </div>
-        </nav>
-      )
+            </nav>
+          }}
+          />
+      );
     }
 }
